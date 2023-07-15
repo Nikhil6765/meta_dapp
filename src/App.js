97568f1262a58d5c3795e437d1939bff1bdb1438
Nikhil6765 <nikhilsondhiya2221@gmail.com>
2023-07-15 12:37:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import WalletCard from './WalletCard';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <WalletCard />
+    </div>
+  );
+}
+
+export default App;
